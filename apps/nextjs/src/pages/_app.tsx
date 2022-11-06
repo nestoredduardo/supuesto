f153e87cc2111a23020ceda6de41b9f8d8a096a4
Nestor Mamani <nmamanipantoja@gmail.com>
2022-11-05 22:08:18 -0500

    feat: ts config

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -1,10 +1,10 @@
 // src/pages/_app.tsx
-import "../styles/globals.css";
-import { SessionProvider } from "next-auth/react";
-import type { Session } from "next-auth";
+import '../styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
+import type { Session } from 'next-auth';
 
-import type { AppType } from "next/app";
-import { trpc } from "../utils/trpc";
+import type { AppType } from 'next/app';
+import { trpc } from '@/utils/trpc';
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
